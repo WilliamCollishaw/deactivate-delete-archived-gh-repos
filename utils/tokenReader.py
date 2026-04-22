674fd0e060c3194f5fa2636ee62cba3917b63890
William Collishaw <williamcollishaw@hotmail.co.uk>
2026-04-21 23:21:08 -0600

    refactor(tokenReader): further refine token reader logic

--- utils/tokenReader.py
+++ utils/tokenReader.py
@@ -7,8 +7,7 @@ def _get_token_from_env(token_name: str) -> str:
     print(f"Checking for {token_name} environment variable")
     token = os.getenv(token_name)
     if not token:
-        print(f"{token_name} does not exist")
-        sys.exit()
+        sys.exit(f"{token_name} does not exist")
     print(f"Found {token_name}")
     return token
 
@@ -16,28 +15,22 @@ def get_snyk_token():
     SNYK_TOKEN = _get_token_from_env('SNYK_TOKEN')
     
     pattern = re.compile(r'snyk_uat\.\S+')
-    if pattern.fullmatch(SNYK_TOKEN) == None:
-        print("Snyk token is not defined or not valid.")
-        sys.exit()
-    else:
-        return SNYK_TOKEN
+    if not pattern.fullmatch(SNYK_TOKEN):
+        sys.exit("Snyk token is not defined or not valid.")
+    return SNYK_TOKEN
 
 def get_gitlab_token():
     GITLAB_TOKEN = _get_token_from_env('GITLAB_TOKEN')
 
     pattern = re.compile(r'glpat-\w{20}')
-    if pattern.fullmatch(GITLAB_TOKEN) == None:
-        print("GitLab token is not defined or not valid.")
-        sys.exit()
-    else:
-        return GITLAB_TOKEN
+    if not pattern.fullmatch(GITLAB_TOKEN):
+        sys.exit("GitLab token is not defined or not valid.")
+    return GITLAB_TOKEN
 
 def get_github_token():
     GITHUB_TOKEN = _get_token_from_env('GITHUB_TOKEN')
 
     pattern = re.compile(r'(ghp_|github_pat_)\w+')
-    if pattern.fullmatch(GITHUB_TOKEN) == None:
-        print("GitHub token is not defined or not valid.")
-        sys.exit()
-    else:
-        return GITHUB_TOKEN
+    if not pattern.fullmatch(GITHUB_TOKEN):
+        sys.exit("GitHub token is not defined or not valid.")
+    return GITHUB_TOKEN
