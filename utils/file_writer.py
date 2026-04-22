f3d5f40d86b0ab76a5299705b54afcb9bd7ac831
William Collishaw <williamcollishaw@hotmail.co.uk>
2026-04-21 23:49:29 -0600

    fix(file_writer): replaces error print with sys.exit

--- utils/file_writer.py
+++ utils/file_writer.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 
 def write_json_to_file(data, file_path):
@@ -8,4 +9,4 @@ def write_json_to_file(data, file_path):
             json.dump(data, json_file, indent=4)
         print(f"Data successfully written to {file_path}")
     except OSError as e:
-        print(f"An error occurred while writing to the file: {e}")
+        sys.exit(f"Error writing to {file_path}: {e}")
