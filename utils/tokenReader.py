71fd1e62bfe5b8bdcccf67f02eec0c57cc0f233c
William Collishaw <williamcollishaw@hotmail.co.uk>
2026-04-21 23:23:40 -0600

    style(tokenReader): linting

--- utils/tokenReader.py
+++ utils/tokenReader.py
@@ -2,6 +2,7 @@ import os
 import re
 import sys
 
+
 def _get_token_from_env(token_name: str) -> str:
     """Retrieve token from environment variable."""
     print(f"Checking for {token_name} environment variable")
@@ -11,26 +12,29 @@ def _get_token_from_env(token_name: str) -> str:
     print(f"Found {token_name}")
     return token
 
+
 def get_snyk_token():
-    SNYK_TOKEN = _get_token_from_env('SNYK_TOKEN')
-    
-    pattern = re.compile(r'snyk_uat\.\S+')
+    SNYK_TOKEN = _get_token_from_env("SNYK_TOKEN")
+
+    pattern = re.compile(r"snyk_uat\.\S+")
     if not pattern.fullmatch(SNYK_TOKEN):
         sys.exit("Snyk token is not defined or not valid.")
     return SNYK_TOKEN
 
-def get_gitlab_token():
-    GITLAB_TOKEN = _get_token_from_env('GITLAB_TOKEN')
 
-    pattern = re.compile(r'glpat-\w{20}')
+def get_gitlab_token():
+    GITLAB_TOKEN = _get_token_from_env("GITLAB_TOKEN")
+
+    pattern = re.compile(r"glpat-\w{20}")
     if not pattern.fullmatch(GITLAB_TOKEN):
         sys.exit("GitLab token is not defined or not valid.")
     return GITLAB_TOKEN
 
-def get_github_token():
-    GITHUB_TOKEN = _get_token_from_env('GITHUB_TOKEN')
 
-    pattern = re.compile(r'(ghp_|github_pat_)\w+')
+def get_github_token():
+    GITHUB_TOKEN = _get_token_from_env("GITHUB_TOKEN")
+
+    pattern = re.compile(r"(ghp_|github_pat_)\w+")
     if not pattern.fullmatch(GITHUB_TOKEN):
         sys.exit("GitHub token is not defined or not valid.")
     return GITHUB_TOKEN
